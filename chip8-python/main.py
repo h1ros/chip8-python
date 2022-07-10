44734b305033781549a8fb86f78bdb6068a30e22
h1ros <data.h1ros@gmail.com>
2022-07-10 14:59:37 -0700

    Implement two opcodes

--- chip8-python/main.py
+++ chip8-python/main.py
@@ -1,34 +1,36 @@
-
 from mychip8 import MyChip8
-
+import time
 
 # Memory
 
-opcode = 0 # unsigned short
-memory = [0 for _ in range(4096)] # unsigned char
+opcode = 0  # unsigned short
+memory = [0 for _ in range(4096)]  # unsigned char
 V = [0 for _ in range(16)]
-I = 0 # unsinged short
-pc = 0 # unsigned short 
+I = 0  # unsinged short
+pc = 0  # unsigned short
 
 gfx = [0 for _ in range(64 * 32)]
-delay_timer = 0 # unsigned char
-sound_timer = 0 # unsigned char
+delay_timer = 0  # unsigned char
+sound_timer = 0  # unsigned char
 
-stack = [0 for _ in range(16)] # unsigned short
-sp = 0 # unsigned short
-key = [0 for _ in range(16)] # unsigned char
+stack = [0 for _ in range(16)]  # unsigned short
+sp = 0  # unsigned short
+key = [0 for _ in range(16)]  # unsigned char
+
 
 def main(argc: int, **argv):
     # Set up render system and register input callbacks
     setup_graphics()
     setup_input()
 
-    # Initialize the Chip8 system and load the game into the memory 
+    # Initialize the Chip8 system and load the game into the memory
     my_chip8 = MyChip8()
-    my_chip8.load_game(game_name='pong')
+    my_chip8.load_game(game_name="rom/pong.rom")
 
     # Emulation loop
     while True:
+        print('Emulation Loop')
+        time.sleep(1)
 
         # Emulate one cycle
         my_chip8.emulate_cycle()
@@ -41,18 +43,17 @@ def main(argc: int, **argv):
         my_chip8.set_keys()
 
 
-
-
-
-
 def setup_graphics():
     pass
+
 
 def setup_input():
     pass
 
+
 def draw_graphics():
     pass
+
 
 if __name__ == "__main__":
     main(0)
--- chip8-python/mychip8.py
+++ chip8-python/mychip8.py
@@ -1,17 +1,61 @@
-
+# fmt: off
+CHIP8_FONTSET = \
+[ 
+  0xF0, 0x90, 0x90, 0x90, 0xF0, ## 0
+  0x20, 0x60, 0x20, 0x20, 0x70, ## 1
+  0xF0, 0x10, 0xF0, 0x80, 0xF0, ## 2
+  0xF0, 0x10, 0xF0, 0x10, 0xF0, ## 3
+  0x90, 0x90, 0xF0, 0x10, 0x10, ## 4
+  0xF0, 0x80, 0xF0, 0x10, 0xF0, ## 5
+  0xF0, 0x80, 0xF0, 0x90, 0xF0, ## 6
+  0xF0, 0x10, 0x20, 0x40, 0x40, ## 7
+  0xF0, 0x90, 0xF0, 0x90, 0xF0, ## 8
+  0xF0, 0x90, 0xF0, 0x10, 0xF0, ## 9
+  0xF0, 0x90, 0xF0, 0x90, 0x90, ## A
+  0xE0, 0x90, 0xE0, 0x90, 0xE0, ## B
+  0xF0, 0x80, 0x80, 0x80, 0xF0, ## C
+  0xE0, 0x90, 0x90, 0x90, 0xE0, ## D
+  0xF0, 0x80, 0xF0, 0x80, 0xF0, ## E
+  0xF0, 0x80, 0xF0, 0x80, 0x80  ## F
+]
+# fmt: on
+BUFFER_SIZE = 4096
 
 
 class MyChip8(object):
 
     draw_flag = 0
+
     def __init__(self):
-        """Initialize registers and memory once
-        """
-        pass
+        """Initialize registers and memory once"""
+        self.pc = 0x200  # program counter starts at 0x200 (=512)
+        self.opcode = 0  # initial opcode
+        self.I = 0  # index register
+        self.sp = 0  # stack pointer
+        self.V = [0] * 16 # register
+        self.memory = [0] * BUFFER_SIZE # memory
 
-    def load_game(self, game_name:str):
-        pass
+        # Clear display
+        # Clear stack
+        # Clear registers V0-VF
+        # Clear memory
 
+        # Load fontset
+        for i in range(0, len(CHIP8_FONTSET)):
+            self.memory[i] = CHIP8_FONTSET[i]
+
+        # Reset timers
+        self.delay_timer = 0
+        self.sound_timer = 0
+
+    def load_game(self, game_name: str):
+
+        with open(game_name, "rb") as f:
+            for i in range(0, BUFFER_SIZE - 512):
+                b = f.read(8) # read one hexadecimal and store as integer
+                if b:
+                    self.memory[i + 512] = int.from_bytes(b, 'big')
+        print(self.memory)
     def set_keys(self):
         pass
 
@@ -20,5 +64,28 @@ class MyChip8(object):
         1. Fetch Opcode
         2. Decode Opcode
         3. Execute Opcode
-        4. Update timers 
+        4. Update timers
         """
+        # Fetch opcode
+        self.opcode = self.memory[self.pc] << 8 | self.memory[self.pc]
+        
+        # Decode opcode
+        if self.opcode & 0xF000 == 0xA000:
+            # ANNN: sets I to the adress NNN
+            self.I = self.opcode & 0x0FFF
+            self.pc += 2
+        elif self.opcode & 0xF000 == 0xB000:
+            # BNNN: jumps to the adress NNN plus V0
+            self.pc = self.V[0] + self.opcode & 0x0FFF
+        else:  
+            print('Unknown opcode: 0x%x\n' % self.opcode)
+
+        # Update timers
+        if (self.delay_timer > 0): 
+            self.delay_timer -= 1
+        if (self.sound_timer > 0): 
+            if self.sound_timer == 1:
+                print('Beep!\n')
+            self.sound_timer -= 1
+                
+            
